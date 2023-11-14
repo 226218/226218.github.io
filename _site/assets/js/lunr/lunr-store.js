d0e648147c0599113edbbe749a27cc28799073bf
226218 <takato_751@hotmail.com>
2023-11-14 05:01:30 -0500

    cambios

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,40 @@ var store = [{
         "title": "Travel - Hack The Box",
         "excerpt":"Travel is an awesome box from my ATeam teammates xct and jkr. The box has a code review part where we analyze the source code of a PHP web app to find a command injection vulnerability in a curl command. We then use the Gopher protocol to perform SSRF and...","categories": ["hackthebox","infosec"],
         "tags": ["vhosts","php","git","memcache","ssrf","gopher","deserialization","ldap","viminfo","nss"],
-        "url": "https://226218.io/htb-writeup-travel/",
-        "teaser":"https://226218.io/assets/images/htb-writeup-travel/travel_logo.png"},{
+        "url": "https://226218.github.io/htb-writeup-travel/",
+        "teaser":"https://226218.github.io/assets/images/htb-writeup-travel/travel_logo.png"},{
         "title": "Appointment - Hack The Box",
         "excerpt":"Appointment is a machine of Starting Point of HTB. The machine has a PHP web app and the objetive is find a command injection vulnerability in the site to claim the flag. 1st HTB VPN connection The initials goals of the HTB in this Starting Point are connect with the...","categories": ["hackthebox","sql_injection"],
         "tags": ["php","mysql","injection","A03:2021"],
-        "url": "https://226218.io/htb-appointment/",
-        "teaser":"https://226218.io/assets/images/htb-appointment/appointment_logo.png"}]
+        "url": "https://226218.github.io/htb-appointment/",
+        "teaser":"https://226218.github.io/assets/images/htb-appointment/appointment_logo.png"},{
+        "title": "Sequel - Hack The Box",
+        "excerpt":"Appointment is a machine of Starting Point of HTB. The machine has a PHP web app and the objetive is find a command injection vulnerability in the site to claim the flag. 1st HTB VPN connection The initials goals of the HTB in this Starting Point are connect with the...","categories": ["hackthebox","sql_injection","MySQL","Databases","Weak Credentials"],
+        "tags": ["mysql","injection","A07:2021"],
+        "url": "https://226218.github.io/htb-sequel/",
+        "teaser":"https://226218.github.io/assets/images/htb-sequel/sequel_logo.png"},{
+        "title": "Crocodile - Hack The Box",
+        "excerpt":"Appointment is a machine of Starting Point of HTB. The machine has a PHP web app and the objetive is find a command injection vulnerability in the site to claim the flag. 1st HTB VPN connection The initials goals of the HTB in this Starting Point are connect with the...","categories": ["hackthebox","FTP","Anynomus Connection","Apache","Weak Credentials"],
+        "tags": ["FTP","Security Misconfiguration","A05:2021"],
+        "url": "https://226218.github.io/htb-crocodile/",
+        "teaser":"https://226218.github.io/assets/images/htb-crocodile/crocodile_logo.png"},{
+        "title": "Responder - Hack The Box",
+        "excerpt":"Appointment is a machine of Starting Point of HTB. The machine has a PHP web app and the objetive is find a command injection vulnerability in the site to claim the flag. 1st HTB VPN connection The initials goals of the HTB in this Starting Point are connect with the...","categories": ["hackthebox","PHP","XAMPP","Password Cracking","NTLM","SMB"],
+        "tags": ["NTLM","LFI","Security Misconfiguration","A05:2021"],
+        "url": "https://226218.github.io/htb-responder/",
+        "teaser":"https://226218.github.io/assets/images/htb-responder/responder_logo.png"},{
+        "title": "Sau - Hack The Box",
+        "excerpt":"Sau is a machine of HTB. The machine has a SSH port open and antoher service TCP, you can doing create an token HTTP to later open a service called Maltrail, later you can search an exploit in this service to claim the flag. 1st HTB VPN connection The initials...","categories": ["hackthebox","PHP","Maltrail","ssh","Token","HTTP"],
+        "tags": ["Vulnerable Components","Exploit CWE","Vulnerable and outdated components","A06:2021"],
+        "url": "https://226218.github.io/htb-sau/",
+        "teaser":"https://226218.github.io/assets/images/htb-sau/sau_logo.png"},{
+        "title": "CozyHosting - Hack The Box",
+        "excerpt":"CozyHosting is a machine of HTB. The machine has a Website with nginx, for this reason has access limited, later u can steal the cookies of an site in actuators/sessions, with the session we can intercept the request to login, later doing using a reverse shell to join like app,...","categories": ["hackthebox","SSH","Interception Credentials","Burp Suite","Weak Credentials","Reverse Shell","Linpeas.sh"],
+        "tags": ["Nginx","HTTP","SSH","burpsuite","Postgresql","Reverse Shell","Privilege Escalation","Security Misconfiguration","A05:2021"],
+        "url": "https://226218.github.io/htb-cozyhosting/",
+        "teaser":"https://226218.github.io/assets/images/htb-cozyhosting/cozyhosting_logo.png"},{
+        "title": "Keeper - Hack The Box",
+        "excerpt":"Keeper is a machine of HTB. The machine has a Website with nginx WAF, for this reason has access limited, later u can join to the system with default credentials, later get the password of an user lisa for the connection ssh, later download the .dmp file and recovery old...","categories": ["hackthebox","SSH","Default Credentials","Nginx","Weak Credentials"],
+        "tags": ["Nginx","HTTP","SSH","Nginx","WAF","dmp credentials","Privilege Escalation","Vulnerable and Outdated Components","A06:2021"],
+        "url": "https://226218.github.io/htb-keeper/",
+        "teaser":"https://226218.github.io/assets/images/htb-keeper/keeper_logo.png"}]
